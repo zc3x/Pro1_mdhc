ea185a13a0fb4f92dda7716ab986301d820e40e8
zc3x <3757390742@qq.com>
2026-03-04 20:15:50 +0800

    这个页面切换的思路有意思

--- js/pay.js
+++ js/pay.js
@@ -1,8 +1,19 @@
-const payBut = document.getElementById("payBut");
-const tip = document.getElementById("tip");
 
-payBut.addEventListener("click",function(){
-  tip.textContent = "支付成功，正在跳转...";
-  payBut.disabled = true;
-  payBut.style.background = "#999";
-});
+
+function toPay() {
+  let pwd = document.getElementById("pwd").value.trim()
+  if (pwd.length !== 6) {
+    alert('密码错误')
+    return;
+  }
+  document.getElementById("payPage").style.display = "none"
+  document.getElementById("loading").style.display = "block"
+
+  setTimeout(() => {
+    document.getElementById("loading").style.display = "none"
+    document.getElementById("success").style.display = "block"
+    setTimeout(() => {
+      location.reload();
+    }, 3000)
+  }, 2000)
+}
